457ef59684a8f96f34eee2ac4b33a8cea5d4cc9c
Joey <101076758+jjjooeyyy@users.noreply.github.com>
2022-08-04 14:57:38 +0100

    Added Reset and Setup grid functions

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
-function drawingBoard(size) {
+const defaultGridSize = 16;
+
+const colorPicker = document.getElementById('colorPicker');
+const eraser = document.getElementById('eraser');
+const reset = document.getElementById('reset');
 let gridSquare = document.querySelector(".gridSquare");
+
+reset.onclick = () => clearGrid();
+
+// Set up grid size, and the drawing function
+function gridSize(size) {
 gridSquare.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
 gridSquare.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
@@ -9,10 +18,16 @@ for(let i=0;i<256;i++) {   // 256 = 16*16
  gridSquare.insertAdjacentElement("beforeend",square);
  square.classList.add("cell"); // the border for each square
  square.addEventListener('mouseover', function(event) { // everytime when mouse over the grid, color will be changed
-    event.target.style.backgroundColor = "black";
+ event.target.style.backgroundColor = "black"; // the default color will be black
  })
 }
 }
 
-drawingBoard(16);
+gridSize(16);
+
+// Reset Function 
+function clearGrid() {
+   gridSquare.innerHTML = ''; // clear color and grid
+   gridSize(defaultGridSize); // set up the grid template again
+}
 
